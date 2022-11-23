2290225dd036190030bd5511339997fb91aca3ad
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-11-23 10:03:24 +0530

    [shravanthi] - Addition

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,6 +7,9 @@ namespace Demo
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            uc1_Addition uc1 = new uc1_Addition();
+            int c = uc1.Addition(2, 3);
+            Console.WriteLine(c);
         }
     }
 }
---
+++ Demo/uc1_Addition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Demo
+{
+    public class uc1_Addition
+    {
+        public int Addition(int a,int b)
+        {
+            return a + b;
+        }
+    }
+}
